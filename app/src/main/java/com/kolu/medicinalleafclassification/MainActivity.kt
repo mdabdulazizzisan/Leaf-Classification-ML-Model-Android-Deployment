f00dbb30f2838992a2834ae381b5b7bb4bc78a3d
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-07-13 06:15:15 +0600

    created general ui
    added view binding
    
    locked screen orientation to potrait

---
+++ app/src/main/java/com/kolu/medicinalleafclassification/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.kolu.medicinalleafclassification
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kolu.medicinalleafclassification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
