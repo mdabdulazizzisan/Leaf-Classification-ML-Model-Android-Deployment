15b0b86dbc3719e0ce8df3a27f527ee7b8be6202
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-07-13 06:55:34 +0600

    added functionality to read image from camera

--- app/src/main/java/com/kolu/medicinalleafclassification/MainActivity.kt
+++ app/src/main/java/com/kolu/medicinalleafclassification/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.kolu.medicinalleafclassification
 
+import android.Manifest
+import android.Manifest.permission.CAMERA
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +17,9 @@ import com.kolu.medicinalleafclassification.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    lateinit var imgBitmap: Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,6 +29,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnCamera.setOnClickListener {takeFromCamera()}
+
+    }
+
+
+    fun takeFromCamera(){
+        if(checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+        } else{
+            val cameraIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, 4704)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 4704){
+            if (data != null) {
+                imgBitmap = data.extras?.get("data") as Bitmap
+                binding.imvLoadedImage.setImageBitmap(imgBitmap)
+                binding.imvLoadedImage.visibility = View.VISIBLE
+            }
+
         }
 
     }
